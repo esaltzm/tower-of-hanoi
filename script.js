cd832013218b1d121c01fb03a325d03ec2c57ff1
esaltzm <elisaltzman@gmail.com>
2022-10-21 16:33:44 -0400

    implemented drag and drop using a different method

--- script.js
+++ script.js
@@ -10,5 +10,19 @@ for(let i = 0; i < nRings; i++) {
     ring.style['background-color'] = 'grey'
     ring.style['width'] = (nRings * 2 - 1 - 2 * i) / (nRings * 2 - 1) * rodWidth + 'px'
     ring.draggable = true
+    ring.addEventListener('dragstart', (event) => {
+        event.dataTransfer.setData("Text", event.target.id)
+    })
     document.querySelector('#rod1').appendChild(ring)
 }
+
+document.addEventListener('dragover', (event) => event.preventDefault())
+document.addEventListener('drop', (event) => {
+    event.preventDefault
+    if(event.target.className == 'rod') {
+        const data = event.dataTransfer.getData("Text");
+        event.target.appendChild(document.getElementById(data))
+    }
+})
+
+// source https://www.w3schools.com/Jsref/event_ondrag.asp
